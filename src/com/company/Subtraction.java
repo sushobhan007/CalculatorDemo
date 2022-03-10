e4f689adbfccded7aab3519042e021b72b619f6d
Sushobhan Mudi <sushobhan712702@gmail.com>
2022-03-10 16:54:32 +0530

    this is the first commit of the project after creation

---
+++ src/com/company/Subtraction.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Subtraction {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int result = 0;
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        if(num1>num2)
+            result = num1 - num2;
+        else
+            result = num2 - num1;
+
+        System.out.println(result);
+    }
+}
