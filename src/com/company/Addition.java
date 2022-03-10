5b1f3dd054f20803a95400607f048131402c6838
Sushobhan Mudi <sushobhan712702@gmail.com>
2022-03-10 17:40:48 +0530

    addition.java file is included

--- src/com/company/Addition.java
+++ src/com/company/Addition.java
@@ -2,16 +2,13 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Subtraction {
+public class Addition {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int result = 0;
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
-        if(num1>num2)
-            result = num1 - num2;
-        else
-            result = num2 - num1;
+        result = num1 + num2;
 
         System.out.println(result);
     }
